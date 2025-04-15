bd76857b3998916d9ef1fb77006f70f950c4a748
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-15 18:16:56 +0545

    added suspense loader for blogs cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
 import prisma from "@/lib/prisma";
+import { Blogcard, SkeletonBlogPostsGrid } from "./mycomponents";
+import { Suspense } from "react";
 
 async function getData() {
   const items = await prisma.blog.findMany({});
   return items;
 }
 
-export default async function Home() {
-  const data = await getData();
+export default function Home() {
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">
         Latest Blog posts
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data?.map((item) => (
-          <p key={item.title}>{item.title}</p>
-        ))}
-      </div>
+      <Suspense fallback={<SkeletonBlogPostsGrid />}>
+        <ShowBlogs />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ShowBlogs() {
+  const data = await getData();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data?.map((item) => (
+        <Blogcard key={item.id} data={item} />
+      ))}
     </div>
   );
 }
