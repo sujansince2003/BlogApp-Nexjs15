d4e53b08cba3552712e6c56e1538a8c6dcb3737f
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-15 18:16:09 +0545

    updated server action for security

--- app/action.ts
+++ app/action.ts
@@ -4,9 +4,13 @@ import prisma from "@/lib/prisma"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from "next/navigation";
 
+
 export async function handleFormSubmission(formData: FormData) {
     const { getUser } = getKindeServerSession();
     const user = await getUser()
+    if (!user) {
+        return redirect("/api/auth/register")
+    }
     const title = formData.get("title")
     const content = formData.get("content")
     const imageurl = formData.get("imageurl")
